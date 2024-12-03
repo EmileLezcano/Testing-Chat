c69a980ee562360239e40bfe6aed2817796a6a6e
FátimaLezcano <151078699+EmileLezcano@users.noreply.github.com>
2024-12-02 22:45:42 -0300

    agregar pruebas unitarias

---
+++ __tests__/server.test.js
@@ -0,0 +1,57 @@
+const request = require('supertest');
+const { app, server, io } = require('../src/server');
+const socketIoClient = require('socket.io-client');
+
+describe('Server Tests', () => {
+    let socket;
+
+    beforeAll((done) => {
+        server.listen(3000, done);
+    });
+
+    afterAll((done) => {
+        io.close();
+        server.close(done);
+    });
+
+    beforeEach((done) => {
+        socket = socketIoClient('http://localhost:3000');
+        socket.on('connect', done);
+    });
+
+    afterEach((done) => {
+        if (socket.connected) {
+            socket.disconnect();
+        }
+        done();
+    });
+
+    test('GET / should return index.html', async () => {
+        const response = await request(app).get('/');
+        expect(response.status).toBe(200);
+        expect(response.type).toBe('text/html');
+    });
+
+    test('Socket should emit chat message', (done) => {
+        socket.on('chat message', (msg) => {
+            expect(msg).toBe('Hello World');
+            done();
+        });
+        socket.emit('chat message', 'Hello World');
+    });
+
+    test('Socket should log disconnect', (done) => {
+        const originalLog = console.log;
+        console.log = jest.fn();
+
+        socket.on('disconnect', () => {
+            setTimeout(() => {
+                expect(console.log).toHaveBeenCalledWith('Un usuario se ha desconectado');
+                console.log = originalLog;
+                done();
+            }, 100); // Espera un breve período para asegurarse de que el evento se maneje
+        });
+
+        socket.disconnect();
+    });
+});
